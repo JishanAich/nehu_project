0f4f1e6d5b45ddff3153da98224d26916e263a75
JishanAich <155349094+JishanAich@users.noreply.github.com>
2025-10-15 23:13:11 +0530

    updatecode

--- MLAPI/.venv/app.py
+++ MLAPI/.venv/app.py
@@ -14,20 +14,17 @@ class prediction(Resource):
     def get(self,budget):
         budget = [int(budget)]
         df = pd.DataFrame(budget, columns=['Marketing Budget (X) in Thousands'])
-        model = pickle.load(open(r"C:\Users\JISHAN AICH\PycharmProjects\MLAPI\.venv\simple_linear_regression.pkl", 'rb'))
+        model = pickle.load(open(r"\MLAPI\.venv\simple_linear_regression.pkl", 'rb'))
         prediction = model.predict(df)
         prediction = int(prediction[0])
         return str(prediction)
 
 class getData(Resource):
     def get(self):
-        df = pd.read_excel(r"C:\Users\JISHAN AICH\PycharmProjects\MLAPI\.venv\data.xlsx")
+        df = pd.read_excel(r"\MLAPI\.venv\data.xlsx")
         df = df.rename({'Marketing Budget (X) in Thousands': 'budget', 'Actual Sales': 'sale'}, axis=1)
         res = df.to_json(orient='records')
         return res
 
 api.add_resource(getData, '/api')
 api.add_resource(prediction, '/prediction/<int:budget>')
-
-if __name__ == '__main__':
-    app.run(debug=True)
